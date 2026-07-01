4ee1b0e48c2a5d4754f3b276f81f928aab9e617a
Pankaj Baria <pankajbaria712@gmail.com>
2026-07-01 20:24:00 +0530

    Remove unused Vercel analytics import causing build failure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
